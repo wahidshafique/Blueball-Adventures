7bc0e1b643b22f04723861720fae1d69aadd8577
RLAshers <kylie.mio@hotmail.com>
2016-12-09 16:59:46 -0500

    DirectionalFix
    I touched up the code so it'll work with any direction the ramp is
    facing.  Scene changes reflect this as you can rotate the entire
    scenario to test it

--- Physics_A2/Assets/Scripts/TargetRamp.cs
+++ Physics_A2/Assets/Scripts/TargetRamp.cs
@@ -107,19 +107,26 @@ public class TargetRamp : MonoBehaviour
     private void NudgeTarget()
     {
         Vector3 rampVector = pRampEndPoint.transform.position - pRampStartPoint.transform.position;
-        mContactPoint.z = 0f;
         float targetDistanceToEnd = (pRampEndPoint.transform.position - mContactPoint).magnitude;
         float rampStrength = pTargetAcceleration * mPlayer.mass;
         float strengthDampener = Mathf.Min((rampVector.magnitude - targetDistanceToEnd) / rampVector.magnitude, 0.1f);
-        rampVector.z = ((pTargetLocation.transform.position - pRampEndPoint.transform.position).z) * Time.fixedDeltaTime;
+        float rampMagnitude = rampVector.magnitude;
         rampVector.y = 0f;
-        Vector3 finalVector = (rampVector.normalized * rampStrength * strengthDampener)
-                              / Time.fixedDeltaTime;
+        Vector3 rampDirection = Vector3.Project(rampVector, this.transform.right).normalized;
+        Vector3 finalVector = (rampDirection * rampStrength * strengthDampener) / Time.fixedDeltaTime;
+
         mPlayer.AddForce(finalVector);
     }
 
     private void LaunchTarget()
     {
+        Vector3 launchPosition = mContactPoint - mPlayer.transform.position;
+
+        if (launchPosition.magnitude > this.transform.localScale.z)
+        {
+            return;
+        }
+
         Vector3 launchVector = pTargetLocation.transform.position - mPlayer.transform.position;
         Vector3 launchVelocity = mPlayer.velocity;
         float accelerationDueToGravity = Physics.gravity.magnitude;
@@ -136,16 +143,7 @@ public class TargetRamp : MonoBehaviour
 
         freeFallTime += timeAfterBuffer;
 
-        float additionalVelocityX = 0f;
-        if (Vector3.Dot(launchVector, transform.right) > 0f)
-        {
-            additionalVelocityX = (launchVector.x / freeFallTime) - launchVelocity.x;
-        }
-        else
-        {
-            Debug.LogWarning("WARNING - Ensure the Launch Target is positioned correctly!!");
-        }
-
+        float additionalVelocityX = (launchVector.x / freeFallTime) - launchVelocity.x;
         float additionalVelocityZ = (launchVector.z / freeFallTime) - launchVelocity.z;
 
         Vector3 additionalMomentum = Vector3.one * mPlayer.mass;
