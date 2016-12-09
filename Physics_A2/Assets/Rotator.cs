351f6413d7d5fd9349ff07020b0bf77792f1c580
Wahid Shafique <wahidshafique@hotmail.com>
2016-12-09 14:59:11 -0500

    seeing if metadata is pushed

---
+++ Physics_A2/Assets/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+    public Rigidbody m_other;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void FixedUpdate() {
+        // if this doesn't work try "B.transform.eulerAngles - A.transform.eulerAngles" below
+        Vector3 difference = m_other.transform.eulerAngles - this.transform.eulerAngles;
+        Vector3 velocity = difference / Time.fixedDeltaTime;
+        m_other.angularVelocity = velocity;
+
+    }
+}
