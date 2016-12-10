b33796e2ee6f8711d4b223b2e4c6da6f8be293cd
CarloAlbino <carloalbino@gmail.com>
2016-12-10 12:46:22 -0500

    Fancied up the levels, cleaned up player controller

--- Physics_A2/Assets/Scripts/PlayerController.cs
+++ Physics_A2/Assets/Scripts/PlayerController.cs
@@ -2,79 +2,47 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+
+    [Tooltip("The y threshold for player death")]
     public float deathY = -10f;
+    [Tooltip("The max speed in all direction")]
     public Vector3 m_maxSpeed = new Vector3(2.0f, 6.0f, 2.0f);
-
+    [Tooltip("Can the player be controlled")]
     public bool m_canMove = true;
-    private Rigidbody m_rb = null;
+    [Tooltip("The spring that the player is attached to ")]
+    public SpringExample m_spring;
 
+    private Rigidbody m_rb = null;
     private bool m_isConnected = false;
     public bool IsConnected {
         set { m_isConnected = value; }
         get { return m_isConnected; }
     }
-    public SpringExample m_spring;
-
     private bool m_isOnIce = false;
     private bool m_isOnGlue = false;
 
-    // Use this for initialization
     void Start() {
         m_rb = GetComponent<Rigidbody>();
         m_isConnected = false;
     }
 
-    void MovePlayer(float xAxis, float zAxis, bool isJumping) {
-        if (m_isConnected) {
-            if (isJumping) {
-                m_spring.DetachFromSpring();
-                m_spring = null;
-                m_isConnected = false;
-            }
-            return;
-        }
-        
-        Vector3 moveVelocity = Vector3.zero;
-        Vector3 xMovement = (xAxis * m_maxSpeed.x * transform.right);
-        Vector3 zMovement = (zAxis * m_maxSpeed.z * transform.forward);
-        moveVelocity = xMovement + zMovement;
-
-        moveVelocity.y = m_rb.velocity.y;
-
-        if (isJumping && (Mathf.Abs(m_rb.velocity.y) < 0.0001f)) {
-            moveVelocity.y = m_maxSpeed.y;
-        }
-
-        if(m_isOnIce)
-        {
-            // this needs to be here so that the player will keep sliding
-            m_rb.AddForce(moveVelocity, ForceMode.Acceleration);
-        }
-        /*else if(m_isOnGlue)
-        {
-            // Take into account the friction
-        }*/
-        else
-        {
-            m_rb.velocity = moveVelocity;
-        }
-
-    }
-
-    // Update is called once per frame
     void Update() {
         if (m_canMove)
         {
+            // If not dead
             if (transform.position.y > deathY)
             {
+                // Grab Input
                 float verticalAxis = Input.GetAxis("Vertical");
                 float horizontalAxis = Input.GetAxis("Horizontal");
                 bool  isJumping = Input.GetKeyDown(KeyCode.Space);
 
+                // Move the player
                 MovePlayer(horizontalAxis, verticalAxis, isJumping);
             }
             else
             {
+                // You DEAD
                 print("YOU DIED BY FALLING");
                 Destroy(this.gameObject);
             }
@@ -83,6 +51,7 @@ public class PlayerController : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
+        // Enter triggers that enable or disable controls
         if(other.CompareTag("DisableControl"))
         {
             m_canMove = false;
@@ -96,6 +65,7 @@ public class PlayerController : MonoBehaviour {
 
     void OnCollisionStay(Collision other)
     {
+        // Check for the what surface the player in on
         if(other.collider.CompareTag("IceFloor"))
         {
             m_isOnIce = true;
@@ -104,5 +74,59 @@ public class PlayerController : MonoBehaviour {
         {
             m_isOnIce = false;
         }
+
+        if (other.collider.CompareTag("GlueFloor"))
+        {
+            m_isOnGlue = true;
+        }
+        else
+        {
+            m_isOnGlue = false;
+        }
+    }
+
+    // Moves the player
+    private void MovePlayer(float xAxis, float zAxis, bool isJumping)
+    {
+        // If connected to a spring only allow jumping off of the spring
+        if (m_isConnected)
+        {
+            if (isJumping)
+            {
+                m_spring.DetachFromSpring();
+                m_spring = null;
+                m_isConnected = false;
+            }
+            return;
+        }
+
+        // Calculate the movement direction
+        Vector3 moveVelocity = Vector3.zero;
+        Vector3 xMovement = (xAxis * m_maxSpeed.x * transform.right);
+        Vector3 zMovement = (zAxis * m_maxSpeed.z * transform.forward);
+        moveVelocity = xMovement + zMovement;
+        moveVelocity.y = m_rb.velocity.y;
+
+        // Set Jumping
+        if (isJumping && (Mathf.Abs(m_rb.velocity.y) < 0.0001f))
+        {
+            moveVelocity.y = m_maxSpeed.y;
+        }
+
+        // Take into account different surfaces
+        if (m_isOnIce)
+        {
+            // this needs to be here so that the player will keep sliding
+            m_rb.AddForce(moveVelocity, ForceMode.Acceleration);
+        }
+        /*else if(m_isOnGlue)
+        {
+            // Take into account the friction
+        }*/
+        else
+        {
+            m_rb.velocity = moveVelocity;
+        }
+
     }
 }
