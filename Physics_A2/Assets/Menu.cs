22783a95b48f07db5b2f5bfe3b0c62b3da45e5a6
Wahid Shafique <wahidshafique@hotmail.com>
2016-12-10 23:33:14 -0500

    woops, forgot to push basic menu

---
+++ Physics_A2/Assets/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public bool manualLoad = false;
+	
+	void Update () {
+		if (manualLoad)
+			SceneManager.LoadScene(0);
+	}
+
+	public void World1 () {
+        SceneManager.LoadScene("World1");
+	}
+    public void World2() {
+        SceneManager.LoadScene("World2");
+    }
+    public void World3() {
+        SceneManager.LoadScene("World3");
+    }
+
+    public void Quit () {
+		Application.Quit();
+	}
+}
